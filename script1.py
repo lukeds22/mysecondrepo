9013883da4f7b90ebcc2566c76a1f5237a964c52
Luke Schmidt <luke_schmidt2@baylor.edu>
2024-09-03 14:54:09 -0500

    fixed mistakes

--- script1.py
+++ script1.py
@@ -4,13 +4,15 @@
 
 def calcTotal(amt,tax,name):
     mytotal = amt * (1+tax)
-    print(f"{myname}, the total of your purchase is ${total:,.2f}")
+    print(f"{name}, the total of your purchase is ${mytotal:,.2f}")
 
-myname = 'Johnny'
-price = 1,560
-thetax = 8.25%
+name = 'Johnny'
+amt = 1560
+tax = .0825
 
-calcTotal(thetax,price,name)
+calcTotal(amt, tax, name)
+
+
 
 
 
